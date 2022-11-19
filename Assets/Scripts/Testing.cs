f3747e106e6178da8ec121460b57e0fe43aa50ce
MNewbigging <mrnewbigging@gmail.com>
2022-11-19 10:05:16 +0000

    pathfinding stuff

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -9,11 +9,22 @@ public class Testing : MonoBehaviour
 
   private void Update()
   {
-    // if (Input.GetKeyDown(KeyCode.T))
-    // {
-    //   List<GridPosition> validPositions = unit.GetMoveAction().GetValidActionGridPositionList();
-    //   GridSystemVisual.Instance.HideAllGridPositions();
-    //   GridSystemVisual.Instance.ShowGridPositionList(validPositions);
-    // }
+    if (Input.GetKeyDown(KeyCode.T))
+    {
+      GridPosition mousePosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+      GridPosition startGridPosition = new GridPosition(0, 0);
+
+      List<GridPosition> gridPositionList = Pathfinding.Instance.FindPath(startGridPosition, mousePosition);
+
+      for (int i = 0; i < gridPositionList.Count - 1; i++)
+      {
+        Debug.DrawLine(
+          LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
+          LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
+          Color.white,
+          10f
+        );
+      }
+    }
   }
 }
